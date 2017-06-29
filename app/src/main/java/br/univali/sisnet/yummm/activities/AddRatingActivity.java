59cbc4d9abeefcd8176e40ca4012f82e6d4390dd
André Vargas <vargas@geosapiens.com.br>
2017-06-29 17:51:59 -0300

    More layout

--- app/src/main/java/br/univali/sisnet/yummm/activities/AddRatingActivity.java
+++ app/src/main/java/br/univali/sisnet/yummm/activities/AddRatingActivity.java
@@ -1,7 +1,11 @@
 package br.univali.sisnet.yummm.activities;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 import br.univali.sisnet.yummm.R;
 
@@ -11,6 +15,12 @@ public class AddRatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_rating);
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        Drawable progress = ratingBar.getProgressDrawable();
+
+        DrawableCompat.setTint(progress, Color.parseColor("#ffd230"));
+
     }
 
 }
